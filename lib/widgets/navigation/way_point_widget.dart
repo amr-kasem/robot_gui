ef6bcb753022d8b70fb28bd907778e04d983d36d
auk-b57 <amr.kasem.b57@gmail.com>
2022-04-30 18:06:24 +0200

    changed marker cursor behaviour

--- lib/widgets/navigation/way_point_widget.dart
+++ lib/widgets/navigation/way_point_widget.dart
@@ -26,6 +26,54 @@ class _WayPointWidgetState extends State<WayPointWidget> {
   bool hover = false;
   @override
   Widget build(BuildContext context) {
+    var _marker = AnimatedScale(
+      duration: const Duration(milliseconds: 150),
+      scale: hover ? 1.1 : 1,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Icon(
+            material.Icons.location_on,
+            color: Colors.blue.normal,
+            size: 40,
+          ),
+          if (widget.editable)
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.red.withOpacity(0.5),
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  icon: const Icon(
+                    FluentIcons.remove,
+                    size: 10,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    widget.onDelete();
+                  },
+                ),
+              ),
+            ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                widget.id.toString(),
+                style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
     return MouseRegion(
       onEnter: (event) {
         setState(() {
@@ -38,87 +86,51 @@ class _WayPointWidgetState extends State<WayPointWidget> {
         });
       },
       cursor: SystemMouseCursors.basic,
-      child: ContextMenuArea(
-        width: 180,
-        verticalPadding: 20,
-        builder: (BuildContext context) => [
-          Shortcuts(
-            shortcuts: {
-              LogicalKeySet(LogicalKeyboardKey.escape): const HideMenuIntent(),
-            },
-            child: Actions(
-              actions: {
-                HideMenuIntent: CallbackAction<HideMenuIntent>(
-                  onInvoke: (HideMenuIntent intent) =>
-                      Navigator.of(context).pop(),
-                ),
-              },
-              child: Focus(
-                autofocus: true,
-                child: Column(
-                  children: [
-                    TappableListTile(
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                      leading: const Icon(FluentIcons.move),
-                      title: const Text('Modify'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-        child: AnimatedScale(
-          duration: const Duration(milliseconds: 150),
-          scale: hover ? 1.1 : 1,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Icon(
-                material.Icons.location_on,
-                color: Colors.blue.normal,
-                size: 40,
-              ),
-              if (widget.editable)
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.red.withOpacity(0.5),
-                      shape: BoxShape.circle,
-                    ),
-                    child: IconButton(
-                      icon: const Icon(
-                        FluentIcons.remove,
-                        size: 10,
-                        color: Colors.white,
+      child: widget.editable
+          ? ContextMenuArea(
+              width: 180,
+              verticalPadding: 20,
+              builder: (BuildContext context) => [
+                Shortcuts(
+                  shortcuts: {
+                    LogicalKeySet(LogicalKeyboardKey.escape):
+                        const HideMenuIntent(),
+                  },
+                  child: Actions(
+                    actions: {
+                      HideMenuIntent: CallbackAction<HideMenuIntent>(
+                        onInvoke: (HideMenuIntent intent) =>
+                            Navigator.of(context).pop(),
                       ),
-                      onPressed: () {
-                        widget.onDelete();
-                      },
+                    },
+                    child: Focus(
+                      autofocus: true,
+                      child: Column(
+                        children: [
+                          TappableListTile(
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                            leading: const Icon(FluentIcons.move),
+                            title: const Text('Modify'),
+                          ),
+                          TappableListTile(
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                            leading:
+                                const Icon(material.Icons.move_down_rounded),
+                            title: const Text('Change index'),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    widget.id.toString(),
-                    style: const TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+              ],
+              child: _marker,
+            )
+          : _marker,
     );
   }
 }
